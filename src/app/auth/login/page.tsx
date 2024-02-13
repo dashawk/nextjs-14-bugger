53062849fdf9e6cb0cbc3cc1a8cde171b1f798e0
Jason P <jmpanugaling@gmail.com>
2024-02-13 22:10:47 +0800

    Bug in NextJS 14

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from '@/components/ui/auth/LoginForm'
+
+const LoginPage = () => {
+  return (
+    <div className="container">
+      <LoginForm />
+    </div>
+  )
+}
+
+export default LoginPage
